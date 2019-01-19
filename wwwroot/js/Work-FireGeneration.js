b1e0a564ad80243cabb5e2173e8aed4f767d9603
Mike <tmal0909@gmail.com>
2019-01-19 16:39:37 +0800

    20190119 時間軸修正

--- wwwroot/js/Work-FireGeneration.js
+++ wwwroot/js/Work-FireGeneration.js
@@ -2,4 +2,13 @@ $(function(){
     // 控制 Header 樣式
     if($('.js-ctrl-background').data('header') == "bright")
         $('.js-style-toggleable, .wrapper').addClass('bright');
+
+    // 時間軸 - 季 - 展開
+    $(document).on('click', '.js-btn-switch', function(){
+       $(this).parent().toggleClass('storyExpand'); 
+    });
+
+    // 時間軸 - 章 - 點選
+    $(document).on('click', '.storyDiv a', function(event){
+    });
 });
